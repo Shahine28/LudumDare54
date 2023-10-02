be60c84d3da59d090983c8fc76f113f51decf73b
pierre <pierrelamare62@gmail.com>
2023-10-02 10:51:53 +0200

    Add anims

---
+++ Assets/Scripts/Scale.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scale : MonoBehaviour
+{
+    [SerializeField] Transform _player;
+    [SerializeField] float _sizeScale = 0.0f;
+
+    private void scale()
+    {
+        
+    }
+}
